8261b0184c602a1d679fd373b24d555095ad7e8c
John Brian <johnbryanjb011@gmail.com>
2022-08-13 13:44:42 +0300

    init Joke list

---
+++ src/JokeList.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+class JokeList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      jokes: [],
+    };
+  }
+
+  componentDidMount() {}
+}
+
+export default JokeList;
