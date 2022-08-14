a3f2949035b2cc0927bdfede193b3dc59c139054
John Brian <johnbryanjb011@gmail.com>
2022-08-14 12:53:58 +0300

    render ten jokes

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,6 +1,12 @@
 import { Component } from "react";
+import axios from "axios";
+
+const JOKE_URL = "https://icanhazdadjoke.com/";
 
 class JokeList extends Component {
+  static defaultProps = {
+    numJokes: 10,
+  };
   constructor(props) {
     super(props);
 
@@ -9,7 +15,28 @@ class JokeList extends Component {
     };
   }
 
-  componentDidMount() {}
+  async componentDidMount() {
+    let fetchedJokes = [];
+    while (fetchedJokes.length < this.props.numJokes) {
+      console.log("fetching jokes");
+      let joke = (
+        await axios.get(JOKE_URL, {
+          headers: { Accept: "application/json" },
+        })
+      ).data;
+
+      console.log("Jokes jokes");
+
+      fetchedJokes.push(joke.joke);
+      console.log(fetchedJokes);
+    }
+    this.setState((st) => ({ jokes: [...st.jokes, ...fetchedJokes] }));
+  }
+
+  render() {
+    const jokes = this.state.jokes.map((j) => <p>{j}</p>);
+    return <div className="JokeList">{jokes}</div>;
+  }
 }
 
 export default JokeList;
